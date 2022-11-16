e79d81ad71c3473e01497326f30df607c9b2260d
Navjot Kukreja <navjotk@gmail.com>
2022-11-16 14:40:40 +0000

    Dict key for user was incorrect

--- run-openmp.sh
+++ run-openmp.sh
@@ -18,7 +18,7 @@ conda activate codegrade
 python ${artifacts_path}/filedownloader.py --file-list ${artifacts_path}/input_files_list.txt
 
 % for submission_dict in submissions:
-python ${artifacts_path}/multithreadrunner.py --basedir ${submission_dict['path']} --max-threads 40 --executable "op1,input_64_512_960.dat,kernel_5.dat,output_64_512_960x5.dat" --executable "op2,a.dat,b.dat,c.dat" --identifier ${submission_dict['id']} --results-file results.csv
+python ${artifacts_path}/multithreadrunner.py --basedir ${submission_dict['path']} --max-threads 40 --executable "op1,input_64_512_960.dat,kernel_5.dat,output_64_512_960x5.dat" --executable "op2,a.dat,b.dat,c.dat" --identifier ${submission_dict['user']} --results-file results.csv
 % endfor
 
 python ${artifacts_path}/cleanup.py --template-file ${artifacts_path}/table.tpl --results-file results.csv --output-file table.html --git-repo ${leaderboard_repo} --freq ${update_frequency}
